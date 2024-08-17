3271075e3b16fd26b2b243be63246ee43b53fcdf
Martyn Cook <martyn@webconnectionz.com>
2024-08-17 14:44:51 +1200

    remove filter for repair

--- pages/products/collections/[collection]/[sub].vue
+++ pages/products/collections/[collection]/[sub].vue
@@ -197,7 +197,7 @@ definePageMeta({
                 You are viewing<br />{{ filteredProducts.length }} of {{ allProducts.length }} products
               </div>
               <div class="flex gap-2">
-                <Button outlined rounded size="small" @click="openDialog" class="w-[12ch] flex-none" label="Filters">
+                <!-- <Button outlined rounded size="small" @click="openDialog" class="w-[12ch] flex-none" label="Filters">
                   <icons-fa-duotone-filters />
                   <span>Filters</span>
                 </Button>
@@ -206,7 +206,7 @@ definePageMeta({
                   <icons-fa-duotone-circle-xmark />
                   <span>Clear Filters</span>
                 </Button>
-                <Divider layout="vertical" />
+                <Divider layout="vertical" /> -->
                 <NuxtLink to="/products/designs"
                   class="relative items-center justify-center inline-flex text-center align-bottom text-sm px-2 py-1 min-w-[2rem] shadow-sm rounded-full bg-transparent dark:text-primary-300 dark:hover:bg-primary-700 dark:hover:text-primary-100 dark:ring-primary-300 dark:hover:ring-primary-100 text-primary-500 ring-1 ring-primary-500 hover:bg-primary-200 hover:text-primary-950 focus:outline-none focus:outline-offset-0 focus:ring-2 focus:ring-offset-current focus:ring-primary-500 dark:focus:ring-primary-400 transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none semi-bold">
                   Explore our designs
