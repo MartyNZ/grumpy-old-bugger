de462f8da7069b1d970ee62636bbf4d048272d02
Martyn Cook <martyn@webconnectionz.com>
2024-12-16 12:03:09 +1300

    Product Feature item alignment #1

--- components/product/featured.vue
+++ components/product/featured.vue
@@ -8,22 +8,18 @@ const { data: featuredProducts } = await useSanityQuery(qryFeaturedProducts);
     <div id="featured-product-carousel" class="flex w-full justify-center">
       <Carousel :value="featuredProducts" :numVisible="1" :numScroll="1" orientation="vertical" circular
         :autoplayInterval="7000">
-        <!-- :autoplayInterval="7000" -->
         <template #item="product">
-          <!-- <pre>{{ product.data }}</pre> -->
           <NuxtLink :to="`/products/${product.data.slug}`">
             <div class="border-1 surface-border border-round m-2 flex flex-col items-center p-3">
-              <div class="mb-3">
-                <div class="relative mx-auto">
-                  <template v-if="product.data.featureImage">
-                    <SanityImage :asset-id="product.data.featureImage.assetId" :alt="product.data.store.title"
-                      class="aspect-square max-w-[200px] rounded-full object-cover shadow" />
-                  </template>
-                  <template v-else>
-                    <img :src="product.data.defaultImageUrl" :alt="product.data.store.title"
-                      class="aspect-square max-w-[200px] rounded-full object-cover shadow" />
-                  </template>
-                </div>
+              <div class="relative mx-auto">
+                <template v-if="product.data.featureImage">
+                  <SanityImage :asset-id="product.data.featureImage.assetId" :alt="product.data.store.title"
+                    class="aspect-square max-w-[200px] rounded-full object-cover shadow" />
+                </template>
+                <template v-else>
+                  <img :src="product.data.defaultImageUrl" :alt="product.data.store.title"
+                    class="aspect-square max-w-[200px] rounded-full object-cover shadow" />
+                </template>
               </div>
               <div class="font-xs mb-3 grow-0 text-balance text-center">
                 {{ product.data.store.title }}
@@ -35,4 +31,8 @@ const { data: featuredProducts } = await useSanityQuery(qryFeaturedProducts);
     </div>
   </section>
 </template>
-<style></style>
+<style>
+.carousel__slide {
+  margin-inline: auto;
+}
+</style>
