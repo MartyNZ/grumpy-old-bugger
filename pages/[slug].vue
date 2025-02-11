630498ce0c5ab3acea95239d68d26c621acd0030
Martyn Cook <martyn@webconnectionz.com>
2025-02-11 14:48:23 +1300

    optimise [slug].vue

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -2,7 +2,10 @@
 import { qryPage } from "~/queries/siteSettings";
 const route = useRoute();
 const slug = route.params.slug;
-const { data: page } = await useSanityQuery(qryPage, { slug: slug });
+const isLoading = ref(true)
+const { data: page } = await useSanityQuery(qryPage, { slug: slug }).finally(() => {
+  isLoading.value = false
+});
 const data = useSiteSettingsStore();
 const settings = data.settings;
 // console.log('Page: ', JSON.stringify(page.value, null, 2));
@@ -10,27 +13,27 @@ const settings = data.settings;
 // console.log("Number of sections: ", JSON.stringify(page.value.sections.length, null, 2));
 
 useSeoMeta({
-  icon: () => settings.logoUrl,
-  title: () => page.value.title,
-  description: () => page.value.description,
-  ogTitle: () => page.value.title,
-  ogDescription: () => page.value.description,
-  // ogImage: () => settings.image,
-  twitterTitle: () => page.value.title,
-  twitterDescription: () => page.value.description,
-  // twitterImage: () => settings.image,
+  icon: computed(() => settings.logoUrl || ''),
+  title: computed(() => page.value.title || ''),
+  description: computed(() => page.value.description || ''),
+  ogTitle: computed(() => page.value.title || ''),
+  ogDescription: computed(() => page.value.description || ''),
+  // ogImage: computed(() => settings.image || ''),
+  twitterTitle: computed(() => page.value.title || ''),
+  twitterDescription: computed(() => page.value.description || ''),
+  // twitterImage: computed(() => settings.image || ''),
   twitterCard: "summary_large_image",
 });
 
-defineOgImageComponent(
-  'default',
-  {
-    title: settings.title,
-    description: settings.description,
-    image: settings.image,
-    siteName: settings.title,
-    icon: settings.logoUrl,
-  });
+const siteSettings = computed(() => ({
+  title: data.settings?.title || '',
+  description: data.settings?.description || '',
+  image: data.settings?.image || '',
+  siteName: data.settings?.title || '',
+  icon: data.settings?.logoUrl || ''
+}))
+
+defineOgImageComponent('default', siteSettings.value)
 
 definePageMeta({
   layout: false,
@@ -41,10 +44,28 @@ definePageMeta({
   <div>
     <NuxtLayout name="default">
       <template #main>
-        <app-page-sections :page="page" />
+        <template v-if="isLoading">
+          <div class="flex flex-col gap-4">
+            <!-- Header skeleton -->
+            <Skeleton height="4rem" class="mb-4" animation="wave" />
+
+            <!-- Content sections skeleton -->
+            <div v-for="n in 3" :key="n" class="mb-6">
+              <Skeleton width="75%" height="2rem" class="mb-2" animation="wave" />
+              <Skeleton height="12rem" animation="wave" />
+            </div>
+          </div>
+        </template>
+        <app-page-sections :page="page" v-else />
       </template>
       <template #sidebar>
-        <product-sidebar />
+        <template v-if="isLoading">
+          <div class="flex flex-col gap-3">
+            <Skeleton width="100%" height="10rem" animation="wave" />
+            <Skeleton width="100%" height="8rem" animation="wave" />
+          </div>
+        </template>
+        <product-sidebar v-else />
       </template>
     </NuxtLayout>
   </div>
