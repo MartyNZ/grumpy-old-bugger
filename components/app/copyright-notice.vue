aac97964d2a18abec80c56c0e1afd2371f09533c
Martyn Cook <martyn@webconnectionz.com>
2025-06-13 19:49:40 +1200

    feat: update copyright notice and branding in footer
    This commit updates the copyright notice in the footer to reflect the current year and changes the "site developed by" link to a "site powered by" link, pointing to PODify+ instead of Web Connectionz.
    
    - Updated the copyright year to the current year.
    - Changed the "site developed by" text to "site powered by".
    - Updated the link in the copyright notice to point to PODify+ instead of Web Connectionz.
    - Updated the link's text color on hover.

--- components/app/copyright-notice.vue
+++ components/app/copyright-notice.vue
@@ -8,20 +8,13 @@ const props = defineProps({
 </script>
 <template>
   <div class="bg-surface-950 text-surface-200 w-full pb-2">
-    <div
-      class=" mx-auto max-w-[1280px] px-6 py-1 text-xs font-semibold md:flex md:items-center md:justify-between"
-    >
+    <div class=" mx-auto max-w-[1280px] px-6 py-1 text-xs font-semibold md:flex md:items-center md:justify-between">
       <div class="py-1 text-center md:text-left">
         <span>© {{ year }} Copyright: </span>{{ siteOwner }}
       </div>
       <div class="py-1 text-center md:text-right">
-        <span>site developed by: </span>
-        <NuxtLink
-          class="hover:text-primary-0"
-          to="https://webconnectionz.com"
-          target="_blank"
-          >Web Connectionz</NuxtLink
-        >
+        <span>site powered by: </span>
+        <nuxtLink class="hover:text-surface-50" to="https://podifyplus.app" target="_blank">PODify+</nuxtLink>
       </div>
     </div>
   </div>
