e7101a7f0a1aad5cc2230f9696804f31ece3ae4a
Martyn Cook <martyn@webconnectionz.com>
2025-07-23 11:55:44 +1200

    fix facebook security issue

--- components/app/social-share.vue
+++ components/app/social-share.vue
@@ -1,8 +1,22 @@
+<script setup>
+const { $config } = useNuxtApp()
+const route = useRoute()
+const fullUrl = computed(() => $config.public.publicUrl + route.fullPath)
+</script>
+
 <template>
   <div class="ml-auto flex flex-row items-center justify-center gap-1">
     <span class="mr-1 text-sm text-primary-900 dark:text-primary-400">Share on:</span>
-    <SocialShare v-for="network in ['facebook', 'linkedin', 'pinterest', 'email']" :key="network" :network="network"
-      :label="false"
-      class="@sm:text-sm @md:px-2 @md:text-base rounded-lg px-1 text-xs hover:text-[color:--color-brand]" />
+    <template v-for="network in ['facebook', 'linkedin', 'pinterest', 'email']" :key="network">
+      <NuxtLink v-if="network === 'facebook'" 
+        :to="`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(fullUrl)}`"
+        target="_blank"
+        class="@sm:text-sm @md:px-2 @md:text-base rounded-lg px-1 text-xs hover:text-[color:--color-brand]">
+        <Icon name="mdi:facebook" />
+      </NuxtLink>
+      <SocialShare v-else :network="network"
+        :label="false"
+        class="@sm:text-sm @md:px-2 @md:text-base rounded-lg px-1 text-xs hover:text-[color:--color-brand]" />
+    </template>
   </div>
 </template>
