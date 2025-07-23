a79d4a3b4a6f3897911888963226175d9c304559
Martyn Cook <martyn@webconnectionz.com>
2025-07-23 19:28:16 +1200

    g

--- components/app/social-share.vue
+++ components/app/social-share.vue
@@ -8,14 +8,12 @@ const fullUrl = computed(() => $config.public.publicUrl + route.fullPath)
   <div class="ml-auto flex flex-row items-center justify-center gap-1">
     <span class="mr-1 text-sm text-primary-900 dark:text-primary-400">Share on:</span>
     <template v-for="network in ['facebook', 'linkedin', 'pinterest', 'email']" :key="network">
-      <NuxtLink v-if="network === 'facebook'" 
-        :to="`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(fullUrl)}`"
-        target="_blank"
+      <NuxtLink v-if="network === 'facebook'"
+        :to="`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(fullUrl)}`" target="_blank"
         class="@sm:text-sm @md:px-2 @md:text-base rounded-lg px-1 text-xs hover:text-[color:--color-brand]">
-        <Icon name="mdi:facebook" />
+        <Icon name="fa:facebook" />
       </NuxtLink>
-      <SocialShare v-else :network="network"
-        :label="false"
+      <SocialShare v-else :network="network" :label="false"
         class="@sm:text-sm @md:px-2 @md:text-base rounded-lg px-1 text-xs hover:text-[color:--color-brand]" />
     </template>
   </div>
